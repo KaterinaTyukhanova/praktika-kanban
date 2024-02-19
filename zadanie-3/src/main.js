cb474c3948f3baa52520ba70d6c9a0d70b336bfe
KaterinaTyukhanova <klimovafiona@gmail.com>
2024-02-19 22:41:20 +0700

    Создание базового шаблона + немного стилизации

--- zadanie-3/src/main.js
+++ zadanie-3/src/main.js
@@ -1,3 +1,34 @@
+Vue.component('board', {
+    template:`
+      <div class="product">
+        
+        <div class="columns-on-page">
+          <div class="column">
+            <h2 class="title-column">Запланированные задачи</h2>
+          </div>
+
+          <div class="column">
+            <h2 class="title-column">Задачи в работе</h2>
+          </div>
+
+          <div class="column">
+            <h2 class="title-column">Тестирование</h2>
+          </div>
+
+          <div class="column">
+            <h2 class="title-column">Выполненные задачи</h2>
+          </div>
+          
+        </div>
+        
+      </div>
+    `,
+    data(){
+        return {}
+    },
+    methods: {}
+})
+
 new Vue({
     el: '#app',
     data(){
